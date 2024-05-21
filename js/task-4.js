7934de4a7c9884df7a68c010b61a3563a601b512
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-05-21 12:34:27 +0300

    add task 4

--- js/task-4.js
+++ js/task-4.js
@@ -12,10 +12,21 @@ function getShippingCost(country) {
       price === 250;
       str = `Shipping to ${country} will cost ${price} credits`;
       break;
+    case 'Australia':
+      price === 170;
+      str = `Shipping to ${country} will cost ${price} credits`;
+      break;
+    case 'Jamaica':
+      price === 120;
+      str = `Shipping to ${country} will cost ${price} credits`;
     default:
       str = 'Sorry, there is no delivery to your country';
   }
   return str;
 }
 console.log(getShippingCost('Australia'));
+console.log(getShippingCost('Germany'));
 console.log(getShippingCost('China'));
+console.log(getShippingCost('Chile'));
+console.log(getShippingCost('Jamaica'));
+console.log(getShippingCost('Sweden'));
